7d810223e96642c541122dce5c2988373db87780
deuk2067 <deuk2067@gmail.com>
2024-07-08 17:06:39 +0900

    IPOproject

---
+++ IPO_Calendar_project/src/main/java/Calendar_Project/IPO_Calendar_project/IpoCalendarProjectApplication.java
@@ -0,0 +1,13 @@
+package Calendar_Project.IPO_Calendar_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpoCalendarProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpoCalendarProjectApplication.class, args);
+	}
+
+}
---
+++ IPO_Calendar_project/src/test/java/Calendar_Project/IPO_Calendar_project/IpoCalendarProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Calendar_Project.IPO_Calendar_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpoCalendarProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
